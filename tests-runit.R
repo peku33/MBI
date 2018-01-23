275efa221ec4c4e9583d3e0e84c8f8ccc75415d7
Paweł Kubrak <peku33@gmail.com>
2018-01-23 21:38:41 +0100

    Dodanie tests-runit.R (test-runnera)

---
+++ tests-runit.R
@@ -0,0 +1,12 @@
+# Na podstawie http://www.johnmyleswhite.com/notebook/2010/08/17/unit-testing-in-r-the-bare-minimum/
+
+library('RUnit')
+
+test.suite <- defineTestSuite(
+	"MBI",
+	".",
+)
+
+test.result <- runTestSuite(test.suite, verbose = TRUE)
+
+printTextProtocol(test.result)
