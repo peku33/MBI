a9f17072429f56ba88132c16ba9ee24fbc2d9867
konrad <konradr25@gmail.com>
2018-02-01 01:26:21 +0100

    Refactoring -> ui moved to another file, algs extracted to functions

--- app.R
+++ app.R
@@ -6,54 +6,7 @@ source('plink.R')
 source('samtools.R')
 source('subsetter.R')
 source('vcfr.R')
-
-
-ui <- fluidPage(
-  shinyjs::useShinyjs(),
-  titlePanel("Loss of heterozygosity - comparison"),
-  
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-    # Sidebar panel for inputs ----
-    sidebarPanel(
- #     fileInput(
-  #      "file1",
-  #      "Choose sequence file",
-  #      multiple = TRUE,
-  #      accept = c("text/csv", # TODO: allow proper format
-  #                 "text/comma-separated-values,text/plain")
-  #    ),
-      
-      textInput("file", "File path", ""),
-      hr(),
-      
-      textInput("sample", "Sample", NULL),
-      textInput("chromosome", "Chromosome number", NULL),
-      textInput("begin", "Begin", NULL),
-      textInput("end", "End", NULL),
-      hr(),
-      helpText("BCFTools ROH params:"),
-      textInput("afDefault", "AF Default", 0.4),
-      textInput("plValue", "PL value", 30.0),
-      textInput("recRate", "Recombination rate", ""),
-      checkboxInput("ignoreHomref", "Skip hom-ref genotypes (0/0)", FALSE),
-      checkboxInput("skipIndels", "Skip indels as their genotypes are enriched for errors", FALSE),
-      textInput("hwToAz", "Transition probability from HW (Hardy-Weinberg) to AZ (autozygous)", NULL),
-      textInput("azToHw", "Transition probability from AZ to HW state", NULL),
-      textInput("viterbiTraining", "Estimate HMM parameters, <float> is the convergence threshold, e.g. 1e-10 (experimental)", NULL),
-      hr(),
-      helpText("PLINK ROH params:"),
-      textInput("minSNPCount", "min SNP count"),
-      textInput("minLength", "min length"),
-      hr(),
-      actionButton('run','Run algorithm'),
-      verbatimTextOutput("progress")
-      
-    ),
-    # Main panel for displaying outputs ----
-    mainPanel(plotOutput("results"),plotOutput("results1"),plotOutput("results2"))
-  )
-)
+source('ui.R')
 
 getObject <- function(input) {
   task <- list()
@@ -69,7 +22,41 @@ getObject <- function(input) {
   task
 }
 
-runBCFToolsROH <- function(output, input, fileName) {
+runBaf <- function(output, input, subsetter.prepare.result) {
+  print("== vcfR BAF ==")
+  vcfr.baf.output <- vcfr.baf.from.file(subsetter.prepare.result)
+  
+  output$results <- renderPlot({
+    #  input$newplotx="position"
+    plot(vcfr.baf.output$position,vcfr.baf.output$baf,col="blue",cex=0.25)
+  })
+}
+
+runPlink <- function(output, input, subsetter.prepare.result) {
+  # Plink params
+  snp <- NULL
+  if (input$minSNPCount > 0) {
+    snp <- input$minSNPCount
+  }
+  kb <- NULL
+  if (input$minLength > 0) {
+    kb <- input$minLength
+  }
+  
+  
+  print("== Plink ROH ==")
+  plink.roh.params <- plink.roh.buildparams(subsetter.prepare.result, snp, kb)
+  print(plink.roh.params)
+  plink.roh.output <- plink.roh.run(plink.roh.params)
+  
+  output$results1 <- renderPlot({
+    #input$newplot
+    plot(plink.roh.output$position, plink.roh.output$homozygosity,col="green",cex=0.25) # mock data
+  })
+  
+}
+
+runBCFToolsROH <- function(output, input, subsetter.prepare.result) {
   # BFCTools params
   af.default = 0.4
   pl.value = 30.0
@@ -90,53 +77,30 @@ runBCFToolsROH <- function(output, input, fileName) {
     rec.rate <- input$recRate
   }
   
-  # Plink params
-  snp <- NULL
-  if (input$minSNPCount > 0) {
-    snp <- input$minSNPCount
-  }
-  kb <- NULL
-  if (input$minLength > 0) {
-    kb <- input$minLength
-  }
+  print("== BCFTools ROH ==")
+  bcftools.roh.params <- bcftools.roh.buildparams(subsetter.prepare.result, af.default, pl.value,	ignore.homref, skip.indels, rec.rate)
+  bcftools.roh.output <- bcftools.roh.run(bcftools.roh.params)
   
+  output$results2 <- renderPlot({
+    #  input$newplot
+    # plot(plink.roh.output) # mock data
+    plot(bcftools.roh.output$position, bcftools.roh.output$homozygosity,col="red",cex=0.25)
+    
+  })
+
+}
+
+runAlgs <- function(output, input, fileName) {
   task <- getObject(input) 
   task$vcf_file_name <- fileName
-  print(task)
-  print(fileName)
   
   print("== subsetter.prepare ==")
   subsetter.prepare.result <- subsetter.prepare(task)
-
-    print("== BCFTools ROH ==")
-    bcftools.roh.params <- bcftools.roh.buildparams(subsetter.prepare.result, af.default, pl.value,	ignore.homref, skip.indels, rec.rate)
-    bcftools.roh.output <- bcftools.roh.run(bcftools.roh.params)
-    
-    print("== Plink ROH ==")
-    plink.roh.params <- plink.roh.buildparams(subsetter.prepare.result, snp, kb)
-    print(plink.roh.params)
-    plink.roh.output <- plink.roh.run(plink.roh.params)
-    
-    
-    print("== vcfR BAF ==")
-    vcfr.baf.output <- vcfr.baf.from.file(subsetter.prepare.result)
-    
-    output$results2 <- renderPlot({
-    #  input$newplot
-     # plot(plink.roh.output) # mock data
-      plot(bcftools.roh.output$position, bcftools.roh.output$homozygosity,col="red",cex=0.25)
-      
-    })
-    
-    output$results1 <- renderPlot({
-      #input$newplot
-      plot(plink.roh.output$position, plink.roh.output$homozygosity,col="green",cex=0.25) # mock data
-    })
-    
-    output$results <- renderPlot({
-    #  input$newplotx="position"
-      plot(vcfr.baf.output$position,vcfr.baf.output$baf,col="blue",cex=0.25)
-    })
+  
+  dataBCF <- runBCFToolsROH(output, input, subsetter.prepare.result)
+  dataPlink <- runPlink(output, input, subsetter.prepare.result)
+  dataBaf <- runBaf(output, input, subsetter.prepare.result)
+  
 }
 
 # Define server logic
@@ -146,13 +110,6 @@ server <- function(input, output) {
   uploadedFilePath <- NULL;
   
   output$value <- renderText({ input$sample })
-  
-#  output$results <- renderPlot({
-#    input$newplot
-#    cars2 <- cars + rnorm(nrow(cars)) # mock data
-#    plot(cars2) # mock data
-#  })
-
   
   output$choosenChromosomes<-renderPrint({
     chromosomes$dList
@@ -165,7 +122,7 @@ server <- function(input, output) {
         "Work in progress"
       })
       
-      data <- runBCFToolsROH(output, input, input$file)
+      runAlgs(output, input, input$file)
       
       output$progress <- renderText({
         "Done"
